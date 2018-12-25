bae2e4e9f4b40936df7be3e5e251b6ad6da707ad
Ray <rmcc3@users.noreply.github.com>
2018-12-25 17:47:50 -0500

    Add files via upload

---
+++ Budget/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.chart = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtHousing = New System.Windows.Forms.TextBox()
+        Me.txtTransport = New System.Windows.Forms.TextBox()
+        Me.txtFood = New System.Windows.Forms.TextBox()
+        Me.txtOther = New System.Windows.Forms.TextBox()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        CType(Me.chart, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'chart
+        '
+        Me.chart.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.chart.Location = New System.Drawing.Point(12, 278)
+        Me.chart.Name = "chart"
+        Me.chart.Size = New System.Drawing.Size(743, 353)
+        Me.chart.TabIndex = 0
+        Me.chart.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Housing:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(105, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Transportation:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(310, 24)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 17)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Food:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(310, 103)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 17)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Other:"
+        '
+        'txtHousing
+        '
+        Me.txtHousing.Location = New System.Drawing.Point(82, 24)
+        Me.txtHousing.Name = "txtHousing"
+        Me.txtHousing.Size = New System.Drawing.Size(158, 22)
+        Me.txtHousing.TabIndex = 5
+        Me.txtHousing.Text = "9700"
+        '
+        'txtTransport
+        '
+        Me.txtTransport.Location = New System.Drawing.Point(123, 103)
+        Me.txtTransport.Name = "txtTransport"
+        Me.txtTransport.Size = New System.Drawing.Size(158, 22)
+        Me.txtTransport.TabIndex = 6
+        Me.txtTransport.Text = "4800"
+        '
+        'txtFood
+        '
+        Me.txtFood.Location = New System.Drawing.Point(360, 21)
+        Me.txtFood.Name = "txtFood"
+        Me.txtFood.Size = New System.Drawing.Size(158, 22)
+        Me.txtFood.TabIndex = 7
+        Me.txtFood.Text = "3900"
+        '
+        'txtOther
+        '
+        Me.txtOther.Location = New System.Drawing.Point(364, 98)
+        Me.txtOther.Name = "txtOther"
+        Me.txtOther.Size = New System.Drawing.Size(158, 22)
+        Me.txtOther.TabIndex = 8
+        Me.txtOther.Text = "6000"
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(636, 24)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(92, 76)
+        Me.btnDisplay.TabIndex = 9
+        Me.btnDisplay.Text = "Display Chart"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(767, 643)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.txtOther)
+        Me.Controls.Add(Me.txtFood)
+        Me.Controls.Add(Me.txtTransport)
+        Me.Controls.Add(Me.txtHousing)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.chart)
+        Me.Name = "Form1"
+        Me.Text = "Budget"
+        CType(Me.chart, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents chart As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtHousing As TextBox
+    Friend WithEvents txtTransport As TextBox
+    Friend WithEvents txtFood As TextBox
+    Friend WithEvents txtOther As TextBox
+    Friend WithEvents btnDisplay As Button
+End Class
---
+++ Budget/Form1.vb
@@ -0,0 +1,38 @@
+﻿' Raymond McCann
+Public Class Form1
+    Dim legend() As String = {"Housing", "Transportation", "Food", "Other"}
+    Dim br() As Brush = {Brushes.Blue, Brushes.Red, Brushes.Green, Brushes.Orange}
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        Dim Housing As Integer = CInt(txtHousing.Text)
+        Dim Transportation As Integer = CInt(txtTransport.Text)
+        Dim Food As Integer = CInt(txtFood.Text)
+        Dim Other As Integer = CInt(txtOther.Text)
+        Dim amounts() As Integer = {Housing, Transportation, Food, Other}
+        Dim gr As Graphics = chart.CreateGraphics
+
+        Dim sum As Decimal = 0
+        Dim sweep As Decimal = 0
+        Dim percent(amounts.Count - 1) As Decimal
+        Dim radius As Integer = 130
+        Dim center As Integer = 135
+
+        For i As Integer = 0 To (amounts.Count - 1)
+            sum += amounts(i)
+        Next
+
+        For i As Integer = 0 To (amounts.Count - 1)
+            percent(i) = amounts(i) / sum
+        Next
+
+        For i As Integer = 0 To (amounts.Count - 1)
+            gr.FillPie(br(i), center - radius, center - radius, 2 * radius, 2 * radius, sweep, percent(i) * 360)
+            sweep += CDec(percent(i) * 360)
+            gr.FillRectangle(br(i), 420, 20 + 30 * i, 20, 20)
+            gr.DrawString(legend(i), Me.Font, Brushes.Black, 450, 22 + 30 * i)
+        Next
+    End Sub
+End Class
